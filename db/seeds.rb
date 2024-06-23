703b9425384af6b6d13dc93002e254c3813c7d4d
mohammed-bageri <mohammed.bajiri.00@gmail.com>
2024-06-23 22:50:03 +0300

    init

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+user = User.find_or_initialize_by(email: "admin@admin.com")
+user.update!(
+  password: "mmoo1418",
+  password_confirmation: "mmoo1418"
+)
