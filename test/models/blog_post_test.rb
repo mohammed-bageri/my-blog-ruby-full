654086e1aa3263661e6a42ddf405c38c5bd1fc38
mohammed-bageri <mohammed.bajiri.00@gmail.com>
2024-06-24 18:44:09 +0300

    add tests

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? is true for draft post" do
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "published? is true for published post" do
+    assert BlogPost.new(published_at: Time.new(2024, 5, 20)).published?
+  end
+
+  test "scheduled? is true for scheduled post" do
+    assert BlogPost.new(published_at: Time.new(2024, 7, 20)).scheduled?
+  end
 end
